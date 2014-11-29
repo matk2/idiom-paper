e5aafb72bf6fec683e8c8c8762eb6f1a89ca0602
Keisuke Matsubara <k.matsu9@gmail.com>
2014-11-29 20:35:35 +0900

    create app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IdiomPaper::Application.config.secret_key_base = 'b1bdc05f5d786eb6706ca66d8304454c451a1f4c007027ecb34d8fa39c07dc336337f290d505a8fc2bab02db16d58ecedd7999af9f00b6d77740b1c5ee303327'
